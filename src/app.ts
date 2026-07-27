aabc7ef584977a5b4c7233cdd31eccf4d64ad348
Lucascardoso255 <lucasrabellocardoso@gmail.com>
2026-07-27 15:52:53 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import Fastify from "fastify";
+
+const fastify = Fastify();
+
+fastify.get('/', async (request, reply) => {
+    reply.send({ hello: "world" });
+})
+
+const start = async () => {
+    try {
+        await fastify.listen( {port: 3000} );
+        console.log("Servidor escutando na porta 3000");
+    }   
+    catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+}
+
+start()
